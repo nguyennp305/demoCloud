3aae701f018b98f57d7e8c515af353c8d9665c03
nguyennp305 <20021406@vnu.edu.vn>
2023-02-20 23:40:18 +0700

    continuous and stop

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,53 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
 
 function MainPage() {
-  // const [continuous, setContinuous] = useState(false);
+  const [numRequests, setNumRequests] = useState(1);
+  const [requestTimes, setRequestTimes] = useState([]);
+  const [averageTime, setAverageTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
 
-  const [data, setData] = useState([]);
-  const [continuous, setContinuous] = useState(false);
-  const [listInterval, setListInterval] = useState([]);
+  useEffect(() => {
+    let intervalId;
 
-  
+    if (isRunning) {
+      intervalId = setInterval(makeRequests, 1000);
+    }
 
-// useEffect(() => {
-//   runRequest();
+    return () => clearInterval(intervalId);
+  }, [isRunning, numRequests]);
 
-// }, [continuous])
+  async function makeRequests() {
+    const url = "http://44.202.179.112:8080/"; // Replace with your API endpoint URL
 
+    let times = [];
 
-const runRequest = () => {
-if (!continuous) {
-  setListInterval([]);
-}
+    for (let i = 0; i < numRequests; i++) {
+      const startTime = performance.now();
 
-  while (continuous) {
-    const interval = setInterval(() => {
-      axios.get('http://3.239.71.115:8080/')
-        .then(response => {
-          setData(response.data);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }, 1000);
-  setListInterval([...listInterval, interval]);
+      fetch(url);
+
+      const endTime = performance.now();
+      const duration = endTime - startTime;
+
+      times.push(duration);
+    }
+
+    const totalDuration = times.reduce((total, duration) => total + duration);
+    const averageDuration = totalDuration / numRequests;
+
+    setRequestTimes((requestTimes) => [...requestTimes, ...times]);
+    setAverageTime(averageDuration);
+    setNumRequests((numRequests) => numRequests * 2);
   }
 
-}
+  function handleChangeStatus() {
+    setIsRunning(!isRunning);
+  }
 
-const changeStatusContinuous = () => {
-setContinuous(!continuous);
-runRequest();
-console.log('ejg')
-
-}
   return (
     <div>
-      <button onClick={changeStatusContinuous}>Start</button>
-      <button onClick={changeStatusContinuous}>Stop</button>
+      <p>
+        Aveage response time for {numRequests} requests: {averageTime.toFixed(2)}ms
+      </p>
+      {<button onClick={handleChangeStatus}>{isRunning ? "Stop" : "Start"}</button>}
     </div>
   );
 }
 export default MainPage;
-
