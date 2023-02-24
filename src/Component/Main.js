cc0b0a7c8b10a6d3f2f0c47ea0c579526e5d15da
nguyennp305 <20021406@vnu.edu.vn>
2023-02-24 21:21:29 +0700

    done

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -2,41 +2,38 @@ import React, { useState, useEffect } from "react";
 
 function MainPage() {
   const [numRequests, setNumRequests] = useState(1);
-  const [requestTimes, setRequestTimes] = useState([]);
-  const [averageTime, setAverageTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
+
+  const [responseTime, setResponseTime] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function makeSingleRequest() {
+    const url = "http://18.214.40.79:8080/"; // Replace with your API endpoint URL
+    setIsLoading(true);
+    const start = Date.now();
+    const response = await fetch(url);
+    const end = Date.now();
+    const responseTime = end - start;
+    setIsLoading(false);
+    setResponseTime(responseTime);
+  }
 
   useEffect(() => {
     let intervalId;
-
     if (isRunning) {
       intervalId = setInterval(makeRequests, 1000);
     }
-
     return () => clearInterval(intervalId);
   }, [isRunning, numRequests]);
 
   async function makeRequests() {
-    const url = "http://44.202.179.112:8080/"; // Replace with your API endpoint URL
-
+    const url = "http://18.214.40.79:8080/"; // Replace with your API endpoint URL
+    setIsLoading(true);
     let times = [];
-
     for (let i = 0; i < numRequests; i++) {
-      const startTime = performance.now();
-
       fetch(url);
-
-      const endTime = performance.now();
-      const duration = endTime - startTime;
-
-      times.push(duration);
     }
-
-    const totalDuration = times.reduce((total, duration) => total + duration);
-    const averageDuration = totalDuration / numRequests;
-
-    setRequestTimes((requestTimes) => [...requestTimes, ...times]);
-    setAverageTime(averageDuration);
+    setIsLoading(false);
     setNumRequests((numRequests) => numRequests * 2);
   }
 
@@ -46,11 +43,15 @@ function MainPage() {
 
   return (
     <div>
-      <p>
-        Aveage response time for {numRequests} requests: {averageTime.toFixed(2)}ms
-      </p>
-      {<button onClick={handleChangeStatus}>{isRunning ? "Stop" : "Start"}</button>}
+      {
+        <button onClick={handleChangeStatus}>
+          {isRunning ? "Stop" : "Start Continuous Request"}
+        </button>
+      }
+      <button onClick={makeSingleRequest}>Send Single Request</button>
+      {isLoading ? <p>Pending...</p> : responseTime && <p>Response time: {responseTime} ms</p>}
     </div>
   );
 }
+
 export default MainPage;
